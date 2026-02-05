1a34721d9e2f1af54e7aba2914b92cc8f08ce2b1
r1ckyIn <rickyqin919@gmail.com>
2026-02-05 17:31:02 +1100

    chore: export resetRedirectFlag for testing

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -2,6 +2,14 @@
  * API module exports.
  */
 
-export { default as apiClient, get, post, put, patch, del } from './client';
+export {
+  default as apiClient,
+  get,
+  post,
+  put,
+  patch,
+  del,
+  resetRedirectFlag,
+} from './client';
 export * from './auth.api';
 export * from './system.api';
